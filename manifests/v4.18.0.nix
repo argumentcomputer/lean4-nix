2470f3da95882130740c5c1abeaf894f3c5cb1df
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2025-06-27 12:00:39 -0400

    v4.11.0

--- manifests/v4.18.0.nix
+++
@@ -1,5 +0,0 @@
-{
-  tag = "v4.18.0";
-  rev = "11ccbced796476be020459a83c599b301a765d3e";
-  inherit (import ./v4.14.0.nix) bootstrap;
-}
--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,4 @@
 let
-  manifests = import ./manifests;
   readSrc = {
     src,
     bootstrap,
@@ -28,20 +27,25 @@ let
     readFromGit {
       args = {
         url = "https://github.com/leanprover/lean4.git";
-        shallow = true;
         ref = "refs/tags/${tag}";
         inherit rev;
       };
       inherit bootstrap;
     };
-  tags = builtins.mapAttrs (tag: manifest: readRev {inherit (manifest) tag rev bootstrap;}) manifests;
   readToolchain = toolchain:
     builtins.addErrorContext "Only leanprover/lean4:{tag} toolchains are supported" (let
       matches = builtins.match "^[[:space:]]*leanprover/lean4:([a-zA-Z0-9\\-\\.]+)[[:space:]]*$" toolchain;
       tag = builtins.head matches;
-    in
-      builtins.getAttr tag tags);
+      tagSrc = builtins.fetchGit {
+        url = "https://github.com/argumentcomputer/lean4-nix.git";
+        ref = "refs/tags/${tag}";
+      };
+      manifest = import (tagSrc.outPath + "/lib/bootstrap.nix");
+      in
+        readRev { inherit (manifest) tag rev bootstrap; }
+    );
+
   readToolchainFile = toolchainFile: readToolchain (builtins.readFile toolchainFile);
 in {
-  inherit readSrc readFromGit readRev tags readToolchain readToolchainFile;
+  inherit readSrc readFromGit readRev readToolchain readToolchainFile;
 }
